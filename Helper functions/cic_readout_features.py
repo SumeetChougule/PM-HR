641f976312341045423e3e823282dd9443d0d0f4
Sumeet Chougule <101477214+SumeetChougule@users.noreply.github.com>
2022-09-10 09:44:21 -0300

    Update cic_readout_features.py

--- Helper functions/cic_readout_features.py
+++ Helper functions/cic_readout_features.py
@@ -50,3 +50,224 @@ def cic_readout_features(mesh, part, name="CiCReadout"):
     value = tf.reduce_sum(weightedvals, axis=-2)
     return value
     
+def cic_paint(mesh, part, weight=None, name="CiCPaint"):
+  """
+  Paints particules on a 3D mesh.
+  Parameters:
+  -----------
+  mesh: tensor (batch_size, nc, nc, nc)
+    Input 3D mesh tensor
+  part: tensor (batch_size, npart, 3)
+    List of 3D particle coordinates, assumed to be in mesh units if
+    boxsize is None
+  weight: tensor (batch_size, npart)
+    List of weights  for each particle
+  """
+  with tf.name_scope(name):
+    mesh = tf.convert_to_tensor(mesh, name="mesh")
+    part = tf.convert_to_tensor(part, name="part")
+    if weight is not None:
+      weight = tf.convert_to_tensor(weight, name="weight")
+
+    shape = tf.shape(mesh)
+    batch_size, nx, ny, nz = shape[0], shape[1], shape[2], shape[3]
+    nc = [nx, ny, nz]
+
+    # Flatten part if it's not already done
+    if len(part.shape) > 3:
+      part = tf.reshape(part, (batch_size, -1, 3))
+
+    # Extract the indices of all the mesh points affected by each particles
+    part = tf.expand_dims(part, 2)
+    floor = tf.floor(part)
+    connection = tf.expand_dims(
+        tf.constant([[[0, 0, 0], [1., 0, 0], [0., 1, 0], [0., 0, 1], [1., 1, 0],
+                      [1., 0, 1], [0., 1, 1], [1., 1, 1]]]), 0)
+
+    neighboor_coords = floor + connection
+    kernel = 1. - tf.abs(part - neighboor_coords)
+    # Replacing the reduce_prod op by manual multiplication
+    # TODO: figure out why reduce_prod was crashing the Hessian computation
+    kernel = kernel[..., 0] * kernel[..., 1] * kernel[..., 2]
+
+    if weight is not None:
+      kernel = tf.multiply(tf.expand_dims(weight, axis=-1), kernel)
+
+    neighboor_coords = tf.cast(neighboor_coords, tf.int32)
+    neighboor_coords = tf.math.mod(neighboor_coords, nc)
+
+    # Adding batch dimension to the neighboor coordinates
+    batch_idx = tf.range(0, batch_size)
+    batch_idx = tf.reshape(batch_idx, (batch_size, 1, 1, 1))
+    b = tf.tile(batch_idx, [1] + list(neighboor_coords.get_shape()[1:-1]) + [1])
+    neighboor_coords = tf.concat([b, neighboor_coords], axis=-1)
+
+    update = tf.scatter_nd(
+        tf.reshape(neighboor_coords, (-1, 8, 4)), tf.reshape(kernel, (-1, 8)),
+        [batch_size, nx, ny, nz])
+    mesh = mesh + update
+    return mesh
+  
+
+def cic_readout(mesh, part, name="CiCReadout"):
+  """
+  Reads out particles from mesh.
+  Parameters:
+  -----------
+  mesh: tensor (batch_size, nc, nc, nc)
+    Input 3D mesh tensor
+  part: tensor (batch_size, npart, 3)
+    List of 3D particle coordinates, assumed to be in mesh units if
+    boxsize is None
+  Return:
+  -------
+  value: tensor (batch_size, npart)
+    Value of the field sampled at the particle locations
+  """
+  with tf.name_scope("CiCReadout"):
+    mesh = tf.convert_to_tensor(mesh, name="mesh")
+    part = tf.convert_to_tensor(part, name="part")
+
+    shape = tf.shape(mesh)
+    batch_size, nx, ny, nz = shape[0], shape[1], shape[2], shape[3]
+    nc = [nx, ny, nz]
+
+    # Flatten part if it's not already done
+    if len(part.shape) > 3:
+      part = tf.reshape(part, (batch_size, -1, 3))
+
+    # Extract the indices of all the mesh points affected by each particles
+    part = tf.expand_dims(part, 2)
+    floor = tf.floor(part)
+    connection = tf.expand_dims(
+        tf.constant([[[0, 0, 0], [1., 0, 0], [0., 1, 0], [0., 0, 1], [1., 1, 0],
+                      [1., 0, 1], [0., 1, 1], [1., 1, 1]]]), 0)
+
+    neighboor_coords = tf.add(floor, connection)
+    kernel = 1. - tf.abs(part - neighboor_coords)
+    kernel = kernel[..., 0] * kernel[..., 1] * kernel[..., 2]
+
+    neighboor_coords = tf.cast(neighboor_coords, tf.int32)
+    neighboor_coords = tf.math.mod(neighboor_coords, nc)
+
+    meshvals = tf.gather_nd(mesh, neighboor_coords, batch_dims=1)
+    weightedvals = tf.multiply(meshvals, kernel)
+    value = tf.reduce_sum(weightedvals, axis=-1)
+    return value
+
+
+def r2c3d(rfield, norm=None, dtype=tf.complex64, name="R2C3D"):
+  """
+  Converts a real field to its complex Fourier Transform
+  Parameters:
+  -----------
+  rfield: tensor (batch_size, nc, nc, nc)
+    Input 3D real field
+  norm: float
+    Normalization factor
+  dtype: tf.dtype
+    Type of output tensor
+  Return:
+  -------
+  cfield: tensor (batch_size, nc, nc, nc)
+    Complex field
+  """
+  with tf.name_scope(name):
+    rfield = tf.convert_to_tensor(rfield, name="mesh")
+    if norm is None:
+      norm = tf.cast(tf.reduce_prod(rfield.get_shape()[1:]), dtype)
+    else:
+      norm = tf.cast(norm, dtype)
+    cfield = tf.multiply(
+        tf.signal.fft3d(tf.cast(rfield, dtype)), 1 / norm, name=name)
+    return cfield
+
+
+def c2r3d(cfield, norm=None, dtype=tf.float32, name="C2R3D"):
+  """
+  Converts a complex Fourier domain field to a real field
+  Parameters:
+  -----------
+  cfield: tensor (batch_size, nc, nc, nc)
+    Complex 3D real field
+  norm: float
+    Normalization factor
+  dtype: tf.dtype
+    Type of output tensor
+  Return:
+  -------
+  rfield: tensor (batch_size, nc, nc, nc)
+    Real valued field
+  """
+  with tf.name_scope(name):
+    cfield = tf.convert_to_tensor(cfield, name="mesh")
+    if norm is None:
+      norm = tf.cast(tf.reduce_prod(cfield.get_shape()[1:]), dtype)
+    else:
+      norm = tf.cast(norm, dtype)
+    rfield = tf.multiply(
+        tf.cast(tf.signal.ifft3d(cfield), dtype), norm, name=name)
+    return rfield  
+  
+  
+def fftk(shape, symmetric=True, finite=False, dtype=np.float64):
+    """ Return k_vector given a shape (nc, nc, nc) and box_size                                                                                                                                                                                                                                                                 
+    """
+    k = []
+    for d in range(len(shape)):
+        kd = np.fft.fftfreq(shape[d])
+        kd *= 2 * np.pi
+        kdshape = np.ones(len(shape), dtype='int')
+        if symmetric and d == len(shape) - 1:
+            kd = kd[:shape[d] // 2 + 1]
+        kdshape[d] = len(kd)
+        kd = kd.reshape(kdshape)
+
+        k.append(kd.astype(dtype))
+    del kd, kdshape
+    return k
+
+
+def laplace_kernel(kvec):
+    kk = sum(ki**2 for ki in kvec)
+    mask = (kk == 0).nonzero()
+    kk[mask] = 1
+    wts = 1. / kk
+    imask = (~(kk == 0)).astype(int)
+    wts *= imask
+    return wts
+ 
+ 
+def gradient_kernel(kvec, direction, order=1):
+    print(direction)
+    wts = 1j * kvec[direction]
+    wts = np.squeeze(wts)
+    wts[len(wts) // 2] = 0
+    wts = wts.reshape(kvec[direction].shape)
+    return wts
+  
+
+def force(delta_k, x, factor=1):
+    
+
+    shape = delta_k.get_shape()
+    nc = shape[1:]
+
+    kvec = fftk(nc, symmetric=False)
+
+    ndim = 3
+    norm = nc[0] * nc[1] * nc[2]
+    lap = tf.cast(laplace_kernel(kvec), tf.complex64)
+    pot_k = tf.multiply(delta_k, lap)
+
+    f = []
+    for d in range(ndim):
+        force_dc = tf.multiply(pot_k, gradient_kernel(kvec, d))
+        forced = c2r3d(force_dc, norm=norm)
+        force = cic_readout(forced, x)
+        f.append(force)
+
+    f = tf.stack(f, axis=2)
+    f = tf.multiply(f, factor)
+    return f  
+  
